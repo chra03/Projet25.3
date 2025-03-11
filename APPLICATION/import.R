16a82c5e18b5a888dd0dca4fff46029d2773c5f8
Aboubacar Amadou-Kaka <aboubacar_amadou-kaka_at_etu_univ-montp3_fr>
2025-03-11 17:25:41 +0100

    Ajout de graphe sur la serie temporelle des voyages à 1 euro avec selection. Inclusion de la map (calculateur de CO2)

---
+++ APPLICATION/import.R
@@ -0,0 +1,21 @@
+source("library.R")
+
+wba <- "SORTIE/"
+wcarto <- "../cartographie_CO2/"
+
+DATA_TEMPO <- read.csv2(paste0(wba, "DATA_TEMPO_1E.csv"), fileEncoding = "latin1")
+unique(DATA_TEMPO$MILLESIME)
+
+DATA_TEMPO23 <- DATA_TEMPO %>%
+  filter(MILLESIME == "2023")
+
+DATA_TEMPO24 <- DATA_TEMPO %>%
+  filter(MILLESIME == "2024")
+
+
+
+GARES_DEP <- c("ENSEMBLE DES PARCOURS", unique(DATA_TEMPO23$Départ))
+
+GARES_ARR <- c("ENSEMBLE DES PARCOURS",unique(DATA_TEMPO23$Arrivée))
+
+names(DATA_TEMPO24)
---
+++ APPLICATION/library.R
@@ -0,0 +1,49 @@
+#Package nécessaire à l'utilisation des scripts#
+#nettoyage 
+library("sf")
+library("stringi")
+library("readxl")
+library("dplyr")
+library("tidyr")
+library("readr")
+library(tidyverse)
+#importation et manipulation de données
+library("data.table")
+library("stringr")
+
+library("tibble")
+
+# Importation rapide
+
+#formats
+library("lubridate")
+
+# Tableau de contingence
+library("questionr")
+
+#Visualisation
+library("ggplot2")
+
+# tableaux
+library("survey")
+library("Hmisc")
+library("plotly")
+
+
+library("viridis")
+library("ggsci")
+library("rainbow")
+library("wesanderson")
+
+
+
+
+
+library("DT")
+library("fresh")
+
+library("shiny")
+
+
+library("shinydashboard")
+library("shinydashboardPlus")
