fb10f99de64a1505c551d6e1bb9ac125bc6422f7
Aboubacar Amadou-Kaka <aboubacar_amadou-kaka_at_etu_univ-montp3_fr>
2025-03-12 14:57:38 +0100

    modif graphique

--- APPLICATION/app.R
+++ APPLICATION/app.R
@@ -1,4 +1,3 @@
-
 source("library.R")
 source("import.R")
 
@@ -37,7 +36,7 @@ ui <- dashboardPage(
     sidebarMenu(
       menuItem("Accueil", tabName = "accueil"),
       menuItem("Analyse", tabName = "analyse"),
-     
+      
       menuItem("Cartographie", tabName = "cartographie"),
       menuItem("Comparateur de CO2", tabName = "comparateur"),
       menuItem("Qui sommes nous ?", tabName = "quisommesnous")
@@ -60,8 +59,8 @@ ui <- dashboardPage(
               # h2("Partie de comparaison(anas)"),
               
               tags$iframe(src = "cartographie/index.html", width = "100%", height = "100%", style = "border: none; height: 100vh; width: 100%;")
-      
-              ),
+              
+      ),
       tabItem(tabName = "cartographie", h2("Partie cartographie")),
       tabItem(tabName = "accueil", h2("?????")),
       tabItem(tabName = "analyse",
@@ -81,15 +80,35 @@ ui <- dashboardPage(
                          solidHeader = TRUE,
                          width = 12,
                          plotlyOutput("graphe1", height = "380px")
+                         
                        )
                 )
-              )
+              ),hr(),hr(),
+              fluidRow(
+                column(3,
+                       selectInput("annee1E", "Choix de l'année", choices = ANNEE1E),
+                       selectInput("mois1E", "Choix du mois", choices = MOIS1E)
+                       
+                ),
+                
+                column(9,
+                       div(
+                         class = "map-container",
+                         tmapOutput("carte_test", height = "600px",width = "auto")
+                       )
+                ) 
+              ),
               
       ),
       tabItem(tabName = "quisommesnous", h2("Présentation"))
     )
   )
 )
+
+
+
+
+
 
 # Serveur
 server <- function(input, output) {
@@ -109,21 +128,25 @@ server <- function(input, output) {
   output$graphe1 <- renderPlotly({
     graphe1 <-  plot_ly() %>%
       add_lines(
-        data = DATA_TEMPO24_react(), 
+        data = DATA_TEMPO24_react()%>%
+          mutate(MOIS = factor(MOIS, levels = c("Janvier","Février","Mars","Avril","Mai","Juin","Septembre","Octobre","Novembre","Décembre")
+, ordered = F)),
         x = ~MOIS, 
-        y = ~Nombre.de.voyages, 
+        y = ~Nombre.de.voyages, name =~MILLESIME,
         hoverinfo = 'x+text',  
-        hovertext = ~paste("jj")
+        hovertext = ~paste(format(Nombre.de.voyages, big.mark = " "))
       ) %>%
       add_lines(
-        data = DATA_TEMPO23_react(), 
+        data = DATA_TEMPO23_react()%>%
+          mutate(MOIS = factor(MOIS, levels = c("Janvier","Février","Mars","Avril","Mai","Juin","Septembre","Octobre","Novembre","Décembre")
+                               , ordered = F)), 
         x = ~as.factor(MOIS), 
-        y = ~Nombre.de.voyages, 
+        y = ~Nombre.de.voyages,  name =~MILLESIME,
         hoverinfo = 'x+text',  
-        hovertext = ~paste("jj")
+        hovertext = ~paste(format(Nombre.de.voyages, big.mark = " "))
       ) %>%
       layout(
-        title = "Tempor",
+        title = "Nombre de personnes ayant effectué le trajet par mois",
         margin = list(l = 50, r = 50, b = 40, t = 80),
         xaxis = list(title = "Mois"),
         yaxis = list(title = "Nombre de personnes"),
@@ -134,6 +157,108 @@ server <- function(input, output) {
     
     graphe1
   })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  df <- read.csv2(paste0(wba, "TAB_1E_POS.csv"), sep = ";", stringsAsFactors = FALSE, fileEncoding = "latin1")
+  
+  # Réactivité pour filtrer les données selon l'année et le mois
+  data_carte <- reactive({
+    req(input$annee1E, input$mois1E)
+    
+    # Nettoyage et séparation des coordonnées
+    df_cleaned <- df %>%
+      mutate(Geom_depart = trimws(Geom_depart),
+             Geom_arrivee = trimws(Geom_arrivee)) %>%
+      separate(Geom_depart, into = c("Lat_Depart", "Lon_Depart"), sep = ",", convert = TRUE) %>%
+      separate(Geom_arrivee, into = c("Lat_Arrivee", "Lon_Arrivee"), sep = ",", convert = TRUE) %>%
+      filter(MILLESIME == input$annee1E, MOIS == input$mois1E) %>%
+      mutate(
+        Lat_Depart = as.numeric(Lat_Depart),
+        Lon_Depart = as.numeric(Lon_Depart),
+        Lat_Arrivee = as.numeric(Lat_Arrivee),
+        Lon_Arrivee = as.numeric(Lon_Arrivee)
+      ) %>%
+      drop_na(Lat_Depart, Lon_Depart, Lat_Arrivee, Lon_Arrivee)  # Supprimer les valeurs NA
+    
+    return(df_cleaned)
+  })
+  
+  # Réactivité pour charger la carte des régions de France
+  france <- ne_states(country = "France", returnclass = "sf") %>%
+    filter(!name %in% c("Guyane française", "Martinique", "Guadeloupe", "La Réunion", "Mayotte")) %>%
+    dplyr :: select(name, region, provnum_ne, geometry)
+  
+  occitanie <- france %>% filter(region == "Occitanie")
+  
+  # Créer la carte
+  output$carte_test <- renderTmap({
+    data <- data_carte()  # Récupérer les données réactives pour les trajets
+    
+    # Créer les objets sf pour les trajets, gares de départ et d'arrivée
+    trajets_sf <- data %>%
+      rowwise() %>%
+      mutate(geometry = list(st_linestring(matrix(c(Lon_Depart, Lat_Depart, Lon_Arrivee, Lat_Arrivee), ncol = 2, byrow = TRUE)))) %>%
+      ungroup() %>%
+      st_as_sf(crs = 4326)  # Définir le système de projection WGS84
+    
+    gares_depart_sf <- data %>%
+      select(Depart, Lat_Depart, Lon_Depart) %>%
+      distinct() %>%
+      st_as_sf(coords = c("Lon_Depart", "Lat_Depart"), crs = 4326)
+    
+    gares_arrivee_sf <- data %>%
+      select(Arrivee, Lat_Arrivee, Lon_Arrivee) %>%
+      distinct() %>%
+      st_as_sf(coords = c("Lon_Arrivee", "Lat_Arrivee"), crs = 4326)
+    
+    # Mode interactif pour la carte tmap
+    tmap_mode("view")
+    
+    # Création de la carte avec OpenStreetMap et la région Occitanie
+    tm_basemap("OpenStreetMap") +
+      tm_shape(occitanie) +
+      tm_polygons(col = "lightgray", alpha = 0.3, border.col = "black", title = "Occitanie") +
+      
+      tm_shape(trajets_sf) +
+      tm_lines(
+        col = "NB_PERS",  # Couleur selon le nombre de voyageurs
+        lwd = 2,
+        palette = "viridis",
+        title.col = "Nombre de voyageurs"
+      ) +
+      
+      tm_shape(gares_depart_sf) +
+      tm_dots(
+        col = "blue", size = 0.5, alpha = 0.8,
+        id = "Depart",  # Afficher le nom de la gare au survol
+        title = "Gares de départ"
+      ) +
+      
+      tm_shape(gares_arrivee_sf) +
+      tm_dots(
+        col = "blue", size = 0.5, alpha = 0.8,
+        id = "Arrivee",  # Afficher le nom de la gare au survol
+        title = "Gares d'arrivée"
+      ) +
+      
+      tm_layout(
+        title = "Carte des trajets à 1€ en Occitanie",
+        legend.outside = TRUE
+      )
+  })
+  
   
   
   
--- APPLICATION/import.R
+++ APPLICATION/import.R
@@ -1,4 +1,9 @@
+
 source("library.R")
+
+
+
+
 
 wba <- "SORTIE/"
 wcarto <- "../cartographie_CO2/"
@@ -18,4 +23,19 @@ GARES_DEP <- c("ENSEMBLE DES PARCOURS", unique(DATA_TEMPO23$Départ))
 
 GARES_ARR <- c("ENSEMBLE DES PARCOURS",unique(DATA_TEMPO23$Arrivée))
 
-names(DATA_TEMPO24)
+# names(DATA_TEMPO24)
+
+
+
+
+
+
+
+
+# # Charger les données depuis un fichier CSV
+df <- read.csv2(paste0(wba, "TAB_1E_POS.csv"), sep = ";", stringsAsFactors = FALSE, fileEncoding = "latin1")
+
+ANNEE1E <- c(unique(df$MILLESIME))
+MOIS1E <- c(unique(df$MOIS))
+
+c("Janvier","Février","Mars","Avril","Mai","Juin","Septembre","Octobre","Novembre","Décembre")
